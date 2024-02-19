5031d80362d484fcc143c649e6d01a399149dfdc
elsalumacang <e.lumacang.61622.dc@umindanao.edu.ph>
2024-02-19 18:14:15 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Double celcius = 23.8;
+        Double fahrenheit = (celcius * 9/5) + 32;
+
+        System.out.println(fahrenheit);
+    
+    }
+
+}
